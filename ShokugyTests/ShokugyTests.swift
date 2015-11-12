f2fdffbdb296db088d92b86d58d85c4d466fcf7d
堀江健太朗 <kentaro.horie.4@gmail.com>
2015-11-13 00:38:24 +0900

    Initial Commit

---
+++ ShokugyTests/ShokugyTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShokugyTests.swift
+//  ShokugyTests
+//
+//  Created by 堀江健太朗 on 2015/11/13.
+//  Copyright © 2015年 Shokugy. All rights reserved.
+//
+
+import XCTest
+@testable import Shokugy
+
+class ShokugyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
