24c952242297e1c562464975eb1d4ea037193f85
堀江健太朗 <kentaro.horie.4@gmail.com>
2015-11-13 16:26:01 +0900

    add images to tabbarItem and other icon

---
+++ Shokugy/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Shokugy
+//
+//  Created by 堀江健太朗 on 2015/11/13.
+//  Copyright © 2015年 Shokugy. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
