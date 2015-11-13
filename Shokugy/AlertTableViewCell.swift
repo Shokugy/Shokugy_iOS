712d126c28ae1509ab8902b7c911549cb0c44b95
堀江健太朗 <kentaro.horie.4@gmail.com>
2015-11-13 23:20:11 +0900

    簡単に設計したもの Tabbarの４つはひとまず簡単につくった

---
+++ Shokugy/AlertTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  AlertTableViewCell.swift
+//  Shokugy
+//
+//  Created by 堀江健太朗 on 2015/11/13.
+//  Copyright © 2015年 Shokugy. All rights reserved.
+//
+
+import UIKit
+
+class AlertTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var alertImage: UIImageView!
+    @IBOutlet weak var alertDetail: UILabel!
+    @IBOutlet weak var alertTime: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Shokugy/AlertTableViewController.swift
+++ Shokugy/AlertTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SearchTableViewController.swift
+//  AlertTableViewController.swift
 //  Shokugy
 //
 //  Created by 堀江健太朗 on 2015/11/13.
@@ -8,16 +8,12 @@
 
 import UIKit
 
-class SearchTableViewController: UITableViewController, UITextFieldDelegate {
+class AlertTableViewController: UITableViewController {
 
-    @IBOutlet weak var searchTextView: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        searchTextView.delegate = self
-        tableView.registerNib(UINib(nibName: "SearchTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
-       
+
+        tableView.registerNib(UINib(nibName: "AlertTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,7 +24,7 @@ class SearchTableViewController: UITableViewController, UITextFieldDelegate {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        何メートル圏内ごとにセクションをわけてもおもしろいかな?
+        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
@@ -36,29 +32,17 @@ class SearchTableViewController: UITableViewController, UITextFieldDelegate {
         // #warning Incomplete implementation, return the number of rows
         return 10
     }
-    
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("customCell") as! SearchTableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("customCell") as! AlertTableViewCell
         
-        cell.layer.borderWidth = 0.1
         cell.layer.cornerRadius = 2
+        cell.layer.borderWidth = 0.1
         
         return cell
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 100
+        return 70
     }
- 
-   
-    @IBAction func tapSearchBtn(sender: UIButton) {
-        print("tap")
-        searchTextView.resignFirstResponder()
-    }
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        searchTextView.resignFirstResponder()
-        return true
-    }
-
 }
