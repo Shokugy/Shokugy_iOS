f269148f9bd9163b4e5bb6490811ecb660cbab94
堀江健太朗 <kentaro.horie.4@gmail.com>
2015-12-20 23:12:19 +0900

    modelファイル導入

---
+++ Shokugy/Comment.swift
@@ -0,0 +1,13 @@
+//
+//  Comment.swift
+//  Shokugy
+//
+//  Created by 堀江健太朗 on 2015/12/20.
+//  Copyright © 2015年 Shokugy. All rights reserved.
+//
+
+import UIKit
+
+class Comment: NSObject {
+
+}
---
+++ Shokugy/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Shokugy
+//
+//  Created by 堀江健太朗 on 2015/12/20.
+//  Copyright © 2015年 Shokugy. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+
+}
