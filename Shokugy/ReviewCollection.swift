fb0c9817048f79bedd284ceceda2c603fc6d56d4
堀江健太朗 <kentaro.horie.4@gmail.com>
2015-12-23 11:20:57 +0900

    ファイル操作
    ファイル名変更
    クラス拡張用ファイル作成

---
+++ Shokugy/ReviewCollection.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewCollection.swift
+//  Shokugy
+//
+//  Created by 堀江健太朗 on 2015/12/22.
+//  Copyright © 2015年 Shokugy. All rights reserved.
+//
+
+import Cocoa
+
+class ReviewCollection: NSObject {
+
+}
---
+++ Shokugy/UINavigationItem+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UINavigationItem+Extension.swift
+//  Shokugy
+//
+//  Created by 堀江健太朗 on 2015/12/23.
+//  Copyright © 2015年 Shokugy. All rights reserved.
+//
+
+import Foundation
